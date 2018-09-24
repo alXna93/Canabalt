c0224971c91f2fbb4d2d5903d319e72d7d033b7b
alXna93 <43002121+alXna93@users.noreply.github.com>
2018-09-24 15:58:56 +0100

    added AssetManager header

---
+++ AssetManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class AssetManager
+{
+
+public: 
+
+	//public interface / behaviours
+
+	//Get a texture based on a file name
+
+private:
+
+	// Private data for this class
+
+	// List of textures
+	// List of sound buffers
+	// List of fonts
+
+};
